5e1c592446cd455ffabb88f9af75102a2d37b08a
narlacloud <55445733+narlacloud@users.noreply.github.com>
2021-10-07 13:51:24 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ resource "aws_subnet" "subnet1-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet1_cidr}"
     availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
 
     tags = {
         Name = "${var.public_subnet1_name}"
